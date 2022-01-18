2145312853a5ce411cd90b6435d3f095d98595a9
Ain Ruiz Dorado <ain.ruiz.dorado@gmail.com>
2022-01-17 23:19:26 -0300

    save course

--- src/app/courses/services/courses.sevices.spec.ts
+++ src/app/courses/services/courses.sevices.spec.ts
@@ -1,6 +1,8 @@
+import { HttpErrorResponse } from "@angular/common/http";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing"
 import { TestBed } from "@angular/core/testing"
 import { COURSES } from "../../../../server/db-data";
+import { Course } from "../model/course";
 import { CoursesService } from "./courses.service"
 
 describe("CoursesService", () => {
@@ -50,6 +52,49 @@ describe("CoursesService", () => {
         const req = httpTestingController.expectOne('/api/courses/12');
         expect(req.request.method).toEqual("GET");
         req.flush(COURSES[12]);
+    })
+
+
+    it('should save the course data', () => {
+
+        const changes : Partial<Course> = {titles:{description: 'Testing Course'}};
+
+        coursesService.saveCourse(12, changes).subscribe(course => {
+
+            expect(course.id).toBe(12);
+
+        })
+
+        const req = httpTestingController.expectOne('/api/courses/12');
+
+        expect(req.request.method).toEqual('PUT');
+
+        expect(req.request.body.titles.description)
+        .toEqual(changes.titles.description);
+
+        req.flush({
+            ...COURSES[12],
+            ...changes
+        })
+
+    });
+
+    it('should give an error if save course fails', () => {
+        const changes : Partial<Course> = {titles:{description: 'Testing Course'}};
+        coursesService.saveCourse(12, changes).subscribe(() => fail('the save course operation should have failed'),
+            (error: HttpErrorResponse) => {
+                expect(error.status).toBe(500);
+            }
+        );
+
+
+
+        const req = httpTestingController.expectOne('/api/courses/12');
+
+        expect(req.request.method).toEqual('PUT');
+
+        req.flush('save course failed', {status: 500, statusText: 'Internal Sever Error'});
+
     })
 
     afterEach(() => {
